bb9e75a3fd7c534c8603706eb4ef9fdf039a2202
bristol <oro.eto.bristol@gmail.com>
2023-03-14 19:59:33 +0200

    added two prototype cells

---
+++ To-Do Manager/View/Cells/TaskCell.swift
@@ -0,0 +1,26 @@
+//
+//  TaskCellTableViewCell.swift
+//  To-Do Manager
+//
+//  Created by Apple M1 on 14.03.2023.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+    
+    @IBOutlet var symbol: UILabel!
+    @IBOutlet var title: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
